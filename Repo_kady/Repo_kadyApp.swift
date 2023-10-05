c5f1c5343f2083d418fed4396f20efa17629ed3c
kadyalyahya <kadyturki4@gmail.com>
2023-10-05 12:52:22 +0300

    Initial Commit

---
+++ Repo_kady/Repo_kadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Repo_kadyApp.swift
+//  Repo_kady
+//
+//  Created by Kady Alyahya on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Repo_kadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
